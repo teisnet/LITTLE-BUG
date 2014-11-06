85339a4d046ff383446c5d53608969d2f5dcfdfa
Teis Draiby <teis@teis.net>
2014-11-06 12:34:19 +0100

    Renaming project til 'LITTLE BUG'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'PUBLIC SPACE ROVER COMMAND CENTER' });
+  res.render('index', { title: 'LITTLE BUG' });
 });
 
 module.exports = router;
